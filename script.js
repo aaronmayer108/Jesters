efbfcaff0fc98b4714a319ce8c3d3aaefc323db5
Aaron Mayer <aaronmayer@gmail.com>
2023-06-05 05:34:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.onload = function() {
+    var fonts = ['"Roboto", sans-serif', '"Open Sans", sans-serif', '"Lato", sans-serif'];
+
     document.getElementById('helloWorldButton').addEventListener('click', function() {
         var newMessage = document.createElement('p');
         newMessage.textContent = 'Hello, World!';
+        
+        var randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+        newMessage.style.fontFamily = randomFont;
+
         document.getElementById('content').appendChild(newMessage);
     });
 }
